d6e67b758abd940c8b7a15c925e85dc41561b2c1
Quintessa <anderson.quintessa@gmail.com>
2019-02-14 18:51:23 -0600

    pushing code for characters cards which now render to the page

--- assets/js/app.js
+++ assets/js/app.js
@@ -37,48 +37,41 @@ const characters = {
 	}
 };
 
-const fruits = {
-		apple: 28, 
-		orange: 17, 
-		pear: 54
-};
-
-
-const fruitEntry = Object.entries(fruits);
-console.log(fruitEntry);
-for(const [fruit, count] of fruitEntry) {
-	console.log(`There are ${count} ${fruit}s`);
-}
-
-console.log(characters);
-console.log(characters.John_Snow.name);
-console.log(characters.John_Snow.healthPoints);
-console.log(characters.John_Snow.attack);
-console.log(characters.John_Snow.image);
-console.log(characters.John_Snow.enemyAttackBack);
-
-
-
-// // Create a function that creates the character cards
-
-function setCharacter (character, cardArea) {
-	console.log(function setCharacter());
-const charEntry = Object.entries(characters);
-console.log(charEntry);
-for(const [character, fightData] of charEntry) {
-	console.log(`Fight Data ${fightData} for ${character}`);
-}
-
+// Create a function that creates the character cards
+function createCharacterCard (character, cardArea) {
+// console.log(setCharacter());
+	// Create a div for each character's card containing name, healthPoints, and image.
 	const $characterDiv = $("<div>", {"class": "characters"});
 	const $characterName = $("<p>");
-	$characterName.setAttribute("class", "characterName");
+	$characterName.attr("class", "characterName");
 	$characterName.text(character.name);
-	const $characterHealthPoints = $("<p>", {"class": "characterHealthPoints"});
+	const $characterHealthPoints = $("<p>");
+	$characterHealthPoints.attr("class", "characterHealthPoints");
 	$characterHealthPoints.text(character.healthPoints);
-	const $characterImage = $("<img>", {"class": "characterImage"});
-	$characterImage.setAttribute("src", character.image);
-
+	const $characterImage = $("<img>");
+	$characterImage.attr("class", "characterImage");
+	$characterImage.attr("src", character.image);
+	$characterDiv.append($characterName).append($characterHealthPoints).append($characterImage);
+	$(cardArea).append($characterDiv);
 }
 
+function setCharacter () {
+	for(var key in characters) {
+		console.log(characters[key]);
+		createCharacterCard(characters[key], "#characterArea");
+	}
+	// const charEntry = Object.entries(characters);
+	// console.log(charEntry);
+	// for(const [character, fightData] of charEntry) {
+	// 	console.log(`Fight Data ${fightData} for ${character}`);
+	// }
+}
+
+setCharacter();
 
 });
+
+
+// var $div = $("<div>", {id: "foo", "class": "a"});
+// $div.click(function(){ /* ... */ });
+// $("#box").append($div);
